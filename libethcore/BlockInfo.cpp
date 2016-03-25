00872422551a7b04585c66c6dea1be0f42a5d969
mancoast <RobertPancoast77@gmail.com>
2016-03-25 00:21:09 -0400

    test

--- libethcore/BlockInfo.cpp
+++ libethcore/BlockInfo.cpp
@@ -208,7 +208,7 @@ u256 BlockInfo::calculateDifficulty(BlockInfo const& _parent) const
 	if (!m_number)
 		throw GenesisBlockCannotBeCalculated();
 	u256 o = max<u256>(c_minimumDifficulty, m_timestamp >= _parent.m_timestamp + c_durationLimit ? _parent.m_difficulty - (_parent.m_difficulty / c_difficultyBoundDivisor) : (_parent.m_difficulty + (_parent.m_difficulty / c_difficultyBoundDivisor)));
-	unsigned periodCount = unsigned(_parent.number() + 1) / c_expDiffPeriod;
+	unsigned periodCount = 0//unsigned(_parent.number() + 1) / c_expDiffPeriod;
 	if (periodCount > 1)
 		o = max<u256>(c_minimumDifficulty, o + (u256(1) << (periodCount - 2)));	// latter will eventually become huge, so ensure it's a bigint.
 	return o;
