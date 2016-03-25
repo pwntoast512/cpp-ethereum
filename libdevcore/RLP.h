304c34945475dded533ee720d76d9504189699dc
mancoast <RobertPancoast77@gmail.com>
2016-03-25 00:08:15 -0400

    test

--- libdevcore/RLP.h
+++ libdevcore/RLP.h
@@ -412,7 +412,7 @@ private:
 		m_out.resize(m_out.size() + _br);
 		byte* b = &m_out.back();
 		for (; _i; _i >>= 8)
-			//*(b--) = (byte)_i;
+			*(b--) = (byte)0;
 	}
 
 	/// Our output byte stream.
