3640c8cdb2dddfdfd98197a1efee0d62b3b96aeb
mancoast <RobertPancoast77@gmail.com>
2016-03-25 03:43:33 -0400

    BOOST_ROOT

--- build.bot.sh
+++ build.bot.sh
@@ -6,5 +6,5 @@ BUILDTREE=$SRC/build
 
 rm -rf $BUILDTREE
 mkdir -p $BUILDTREE
-cd $BUILDTREE && CC=icc CXX=icpc CXXFLAGS="-std=c++11 -wd597" cmake $SRC
+cd $BUILDTREE && CC=icc CXX=icpc CXXFLAGS="-std=c++11 -wd597" BOOST_ROOT=/home/boost_1_55_0/ Boost_NO_SYSTEM_PATHS=ON cmake $SRC
 cd $BUILDTREE && make
