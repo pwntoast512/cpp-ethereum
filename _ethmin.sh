92ea938132cf86878e26b03b99815b85be4d2849
mancoast <RobertPancoast77@gmail.com>
2016-03-24 20:48:05 -0400

    micro miner

--- _ethmin.sh
+++ _ethmin.sh
@@ -3,11 +3,9 @@
 # Build Systems #
 SRC=/home/cpp-ethereum
 BUILDTREE=$SRC/build
-BINDIR=$BUILDTREE/libethereum/ethminer
+BINDIR=$BUILDTREE/ethminer
 
 $BINDIR/ethminer -v 9 \
 --farm-recheck 400 \
---cl-global-work 16384 \
---cl-local-work 256 \
--G -F \
+-F \
 http://192.168.1.77:8080/rig1
