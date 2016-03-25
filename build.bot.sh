cc9212ac50e497df812f192cb93cd36398746877
mancoast <RobertPancoast77@gmail.com>
2016-03-25 00:00:59 -0400

    try new cxxvars

--- build.bot.sh
+++ build.bot.sh
@@ -6,5 +6,5 @@ BUILDTREE=$SRC/build
 
 rm -rf $BUILDTREE
 mkdir -p $BUILDTREE
-cd $BUILDTREE && CC=icc CXX=icpc CXXFLAGS="-std=c++11 -wd597" cmake $SRC
+cd $BUILDTREE && CC=icc CXX=icpc CXXFLAGS="-std=c++11 -Wall -Wno-unknown-pragmas -Wextra -Wno-error=parentheses -pedantic -DSHAREDLIB -fPIC  -fuse-ld=gold -wd597" cmake $SRC
 cd $BUILDTREE && make
