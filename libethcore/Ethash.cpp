6c39e0115701c0bf3a6741b8f12fd8699f1c3a09
mancoast <RobertPancoast77@gmail.com>
2016-03-25 00:50:41 -0400

    test

--- libethcore/Ethash.cpp
+++ libethcore/Ethash.cpp
@@ -125,7 +125,7 @@ bool Ethash::BlockHeaderRaw::preVerify() const
 
 	bool ret = !!ethash_quick_check_difficulty(
 			(ethash_h256_t const*)hashWithout().data(),
-			(uint64_t)(u64)0,
+			(uint64_t)0,
 			(ethash_h256_t const*)m_mixHash.data(),
 			(ethash_h256_t const*)boundary().data());
 	return ret;
